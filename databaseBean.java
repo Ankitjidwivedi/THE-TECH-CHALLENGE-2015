db9ca3b72deea20e9492115a0e363d051f3453a1
Ankit Ji Dwivedi <ankitdwivedi.41@gmail.com>
2015-05-28 17:03:28 +0530

    Create databaseBean.java

---
+++ databaseBean.java
@@ -0,0 +1,27 @@
+package com.dataparser;
+ 
+public class databaseBean {
+ 
+	private String networksId;
+	private String networksTitle;
+	private String networksCode;
+	
+	public String getNetworksId() {
+		return networksId;
+	}
+	public void setNetworksId(String networksId) {
+		this.networksId = networksId;
+	}
+	public String getNetworksTitle() {
+		return networksTitle;
+	}
+	public void setNetworksTitle(String networksTitle) {
+		this.networksTitle = networksTitle;
+	}
+	public String getNetworksCode() {
+		return networksCode;
+	}
+	public void setNetworksCode(String networksCode) {
+		this.networksCode = networksCode;
+	}
+}
